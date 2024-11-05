aa87cb2ae1a1e98abf5cf98b8252bf38b42b66d5
aminetiouassiouinee <aminetiouassiouinee@gmail.com>
2024-11-05 17:37:57 +0100

    nouveau fichier : page-single.html 	nouveau fichier : script.js 	nouveau fichier : style.css

---
+++ script.js
@@ -0,0 +1,21 @@
+// Product image > page single
+const thumbImage = new Swiper('.thumbnail-image', {
+    logo: true,
+    direction: 'vertical',
+    spaceBetween: 15,
+    slidesPerView: 1, // Ajustez le nombre d'images visibles
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+const mainImage = new Swiper('.main-image', {
+   	logo: true,
+    autoHeight: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    thumbs: {
+        swiper: thumbImage, // Lien vers les vignettes
+    },
+});
